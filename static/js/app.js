65a3b22133f91685cbed07be6fe8d230276b737d
Kyle-McGitHub <160557276+Kyle-McGitHub@users.noreply.github.com>
2024-06-11 17:28:09 -0400

    Full Revert of Sample Code

--- static/js/app.js
+++ static/js/app.js
@@ -1,24 +1,22 @@
 // Build the metadata panel
-function buildMetadata(id) {
+function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
-    // Get the metadata field
-    let metadata = data.metadata;
+    // get the metadata field
+
 
     // Filter the metadata for the object with the desired sample number
-    let resultArray = metadata.filter(sampleObj => sampleObj.id == "id");
-    let result = resultArray[0];
+
 
     // Use d3 to select the panel with id of `#sample-metadata`
-    let panel = d3.select("#sample-metadata");
+
 
     // Use `.html("") to clear any existing metadata
-    panel.html("");
 
-    // Inside a loop, use d3 to append new tags for each key-value in the filtered metadata
-    Object.entries(result).forEach(([key, value]) => {
-      panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
-    });
+
+    // Inside a loop, you will need to use d3 to append new
+    // tags for each key-value in the filtered metadata.
+
   });
 }
 
@@ -26,60 +24,31 @@ function buildMetadata(id) {
 function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
-   // Get the samples field
-   let samples = data.samples;
+    // Get the samples field
 
-   // Filter the samples for the object with the desired sample number
-   let sampleData = samples.filter(sampleObj => sampleObj.id == sample)[0];
 
-   // Get the otu_ids, otu_labels, and sample_values
-   let otuIds = sampleData.otu_ids;
-   let otuLabels = sampleData.otu_labels;
-   let sampleValues = sampleData.sample_values;
+    // Filter the samples for the object with the desired sample number
 
-   // Build a Bubble Chart
-   let bubbleChartTrace = {
-     x: otuIds,
-     y: sampleValues,
-     text: otuLabels,
-     mode: 'markers',
-     marker: {
-       size: sampleValues,
-       color: otuIds,
-       colorscale: 'Earth'
-     }
-   };
 
-   let bubbleChartData = [bubbleChartTrace];
+    // Get the otu_ids, otu_labels, and sample_values
 
-   let bubbleChartLayout = {
-     title: 'Bubble Chart',
-     xaxis: { title: 'OTU ID' },
-     yaxis: { title: 'Sample Values' }
-   };
 
-   Plotly.newPlot('bubble-chart', bubbleChartData, bubbleChartLayout);
+    // Build a Bubble Chart
 
-   // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-   let yticks = otuIds.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
 
-   // Build a Bar Chart
-   let barChartData = [{
-     type: 'bar',
-     x: sampleValues.slice(0, 10).reverse(),
-     y: yticks,
-     text: otuLabels.slice(0, 10).reverse(),
-     orientation: 'h'
-   }];
+    // Render the Bubble Chart
 
-   let barChartLayout = {
-     title: 'Top 10 OTUs',
-     xaxis: { title: 'Sample Values' },
-     yaxis: { title: 'OTU ID' }
-   };
 
-   Plotly.newPlot('bar-chart', barChartData, barChartLayout);
- });
+    // For the Bar Chart, map the otu_ids to a list of strings for your yticks
+
+
+    // Build a Bar Chart
+    // Don't forget to slice and reverse the input data appropriately
+
+
+    // Render the Bar Chart
+
+  });
 }
 
 // Function to run on page load
